f9c1d5654bda3bb5543b1e50833df5ab03c5143e
root <root@lucid-action-1.localdomain>
2022-11-11 12:13:14 +0000

    push

---
+++ delete_static_index.sh
@@ -0,0 +1,5 @@
+rm -r /var/www/html/dist/static
+rm /var/www/html/dist/index.html
+cp -r /root/blog/blog_front/dist/static /var/www/html/dist/
+cp /root/blog/blog_front/dist/index.html /var/www/html/dist/
+
